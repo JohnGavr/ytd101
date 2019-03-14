3f214ad470b9cbbf4fbfe97b4d196da1725bdfed
JohnGavr <47471932+JohnGavr@users.noreply.github.com>
2019-03-14 13:37:56 +0200

    Add files via upload

---
+++ youtubeb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Πρόγραμμα για λήψη mp3 αρχείων του Youtube
+
+read -rp "Youtube URL:" video
+
+if [[ -z $video ]] 
+   then
+   echo "Σφάλμα URL"
+ exit 0
+else 
+  youtube-dl -x --audio-format mp3 $video 
+fi
+
+exit 0
