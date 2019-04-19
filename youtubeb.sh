3efba674395ef2134f875dfc5ce2ff4281b1f226
Ioannis Gavriilidis <gavriilidisg@tutanota.com>
2019-04-19 23:53:43 +0300

    New URL accepted

--- youtubeb.sh
+++ youtubeb.sh
@@ -2,9 +2,9 @@
 
 # Πρόγραμμα για λήψη mp3 αρχείων του Youtube
 
-read -rp "Youtube URL:" video
+read -rp "Youtube URL:" url
 
-if [[ $video = 'https://www.youtube.com/watch?'*v=* ]] 
+if [[ $url = 'https://www.youtube.com/watch?'*v=* ]] || [[ $url == 'https://youtube.com/watch?'*v=* ]] || [[ $url == 'https://youtu.be'*/* ]] || [[ $url == 'm.youtube.com'** ]]
    then
    youtube-dl --extract-audio --audio-format mp3 --embed-thumbnail --output "%(title)s.%(ext)s" $video 
    exit 0
