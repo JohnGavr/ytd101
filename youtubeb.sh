e557e5b9da2259f16d6dda425c3db9d2aec7f83b
Ioannis Gavriilidis <gavriilidisg@tutanota.com>
2019-04-19 23:56:15 +0300

    Variable problem sovled

--- youtubeb.sh
+++ youtubeb.sh
@@ -6,7 +6,7 @@ read -rp "Youtube URL:" url
 
 if [[ $url = 'https://www.youtube.com/watch?'*v=* ]] || [[ $url == 'https://youtube.com/watch?'*v=* ]] || [[ $url == 'https://youtu.be'*/* ]] || [[ $url == 'm.youtube.com'** ]]
    then
-   youtube-dl --extract-audio --audio-format mp3 --embed-thumbnail --output "%(title)s.%(ext)s" $video 
+   youtube-dl --extract-audio --audio-format mp3 --embed-thumbnail --output "%(title)s.%(ext)s" $url 
    exit 0
 else 
   echo "This is not a youtube link"
